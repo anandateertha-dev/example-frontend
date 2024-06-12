3946b85fdb4dbc1a9a37d3620fa64a22dd2f25fb
Anandateertha.dev <vortex1649@gmail.com>
2024-06-12 11:40:54 +0530

    Change

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Todos = () => {
 
@@ -23,6 +23,11 @@ const Todos = () => {
 
     return (
         <>
+            <h1>Video playing</h1>
+            <video controls width="600">
+                <source src="/3571264-uhd_3840_2160_30fps.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
             <div className="App">
                 <h1>Todos Fetched</h1>
                 <ul>
